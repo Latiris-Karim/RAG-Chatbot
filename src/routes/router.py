e555e4995d24cd16966e79bd1a8bb8bf260b8b69
Karim Latiris <latiriskarim7@gmail.com>
2025-06-06 19:26:03 +0200

    payment router

--- src/routes/router.py
+++ src/routes/router.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
 from src.routes import chat_routes
 from src.routes import user_routes
+from src.routes import payment_routes
 
 router = APIRouter()
+
 router.include_router(chat_routes.router)
 router.include_router(user_routes.router)
+router.include_router(payment_routes.router)
