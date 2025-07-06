30d9f9221b9cf2798e7c17c63ed1b7b5baff0cfa
Karim Latiris <latiriskarim7@gmail.com>
2025-07-06 14:22:55 +0200

    shortened password to pw, adjusted endpoints in more intuitive order

--- src/routes/settings_routes.py
+++ src/routes/settings_routes.py
@@ -6,11 +6,11 @@ from src.db.settings_db import change_pw, user_info, reset_password, forgot_pw, 
 router = APIRouter(prefix="/settings", tags=["settings"])
 
 class PwChange(BaseModel):
-    password: str
+    pw: str
 
 class PwForgot(BaseModel):
     token: str
-    password: str
+    pw: str
 
 class PwRequest(BaseModel):
     email: str
@@ -28,31 +28,31 @@ async def display(u_id: int = Depends(get_current_user)):
 
 @router.post('/change_pw')#from logged in user
 async def change_current_pw(req: PwChange, u_id: int = Depends(get_current_user)):
-    return change_pw(u_id, req.password)
+    return change_pw(u_id, req.pw)
 
-@router.post('/request_pw_change')#send url for pw change to user-email
+@router.post('/request_pw_change')#send url for pw change to user-email (step 1)
 async def pw_request(req: PwRequest):
     email = req.email
     return forgot_pw(email)
 
-@router.post('/change_forgotten_pw')#validate token in url then change pw 
+@router.post('/change_forgotten_pw')#validate token in url then change pw (step 2)
 async def change_forgotten_pw(req: PwForgot):
     reset_token = req.token
-    new_pw = req.password
+    new_pw = req.pw
     return reset_password(reset_token, new_pw)
 
-@router.post('/delete_account')
-async def delete_user_account(u_id: int = Depends(get_current_user)):
-    res = delete_account(u_id)
-    return res
 
-@router.post('/change_email_request')
+@router.post('/change_email_request') #step 1 
 async def change_email_req(req: NewEmailRequest,u_id: int = Depends(get_current_user)):
     res = change_email_request(u_id,req.newEmail)
     return res
 
-
-@router.post('/change_email')
+@router.post('/change_email') #step2
 async def change_user_emailreq(req: NewEmailChange, u_id:int = Depends(get_current_user)):
     res = change_email(u_id, req.code)
+    return res
+
+@router.post('/delete_account')
+async def delete_user_account(u_id: int = Depends(get_current_user)):
+    res = delete_account(u_id)
     return res
