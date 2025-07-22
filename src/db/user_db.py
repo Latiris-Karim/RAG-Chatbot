25c354d4ecb21c6d2d54306f337c9027ed9d0156
Karim Latiris <latiriskarim7@gmail.com>
2025-07-22 14:14:32 +0200

    tokenpair login adjustment

--- src/db/user_db.py
+++ src/db/user_db.py
@@ -3,13 +3,13 @@ import os
 import random
 import string
 from email_validator import validate_email, EmailNotValidError
+from datetime import datetime, timedelta
 
 if __name__ == "__main__":  #to test without api calls 
     sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
 from src.config.database import Database
 import bcrypt
-from src.utils.jwt_handler import create_token
 from src.services.email_service import Emails
 
 db = Database()
@@ -67,12 +67,15 @@ def login(email: str, pw: str):
     user_verified = user_status(user_id)
 
     if bcrypt.checkpw(pw.encode('utf-8'), stored_hash.encode('utf-8')) and user_verified == True:
-        token = create_token(user_id)
+        from src.utils.jwt_handler import create_token_pair
+        tokens = create_token_pair(user_id)
         
         return {
             "message": "Login successful", 
             "user_id": user_id,
-            "token": token
+            "access_token": tokens["access_token"],
+            "refresh_token": tokens["refresh_token"],
+            "token_type": tokens["token_type"]
         }
     else:
         if user_verified == False:
@@ -101,3 +104,28 @@ def get_customer_id(u_id):
     return None
 
    
+def store_refresh_token(u_id, token_hash):
+    delete_query = 'DELETE FROM refresh_tokens WHERE user_id = %s'
+    db.execute_query(delete_query, (u_id,))
+    
+    insert_query = '''
+        INSERT INTO refresh_tokens (token_hash, user_id, expires_at, created_at)
+        VALUES (%s, %s, %s, %s)
+    '''
+    db.execute_query(insert_query, (token_hash, u_id, datetime.utcnow() + timedelta(days=7), datetime.utcnow()))
+
+
+def get_stored_refresh_token(u_id):
+    select_query = '''SELECT token_hash FROM refresh_tokens WHERE user_id = %s'''
+    result = db.fetch_query(select_query, (u_id,))
+    return result[0]['token_hash']
+
+def delete_refresh_token(user_id: int):
+    """Delete refresh token on logout"""
+    delete_query = 'DELETE FROM refresh_tokens WHERE user_id = %s'
+    db.execute_query(delete_query, (user_id,))
+
+def get_user_by_refresh_token(hash_token):
+    select_query = '''SELECT user_id FROM refresh_tokens WHERE token_hash = %s'''
+    result = db.fetch_query(select_query,(hash_token,))
+    return result[0]['user_id']
