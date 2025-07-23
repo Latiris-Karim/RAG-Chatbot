1cae523498d8f342279cef4bb2216813242bf954
Karim Latiris <latiriskarim7@gmail.com>
2025-07-23 17:49:48 +0200

    client created to test fastapi app endpoints with a fake auth header and real header, will be added at some point

---
+++ ragapi/tests/conftest.py
@@ -0,0 +1,14 @@
+from src.main import app
+from fastapi.testclient import TestClient
+from src.utils.jwt_handler import create_token_pair
+
+
+client = TestClient(app)
+
+def fake_auth_header(token="test_token"):
+    return {"Authorization": f"Bearer {token}"}
+
+def real_auth_header(u_id=2):
+    tokens = create_token_pair(u_id)
+    access_token = tokens["access_token"]  
+    return {"Authorization": f"Bearer {access_token}"}
